27f439c3d7aea01d5bd693e054943023ca5258d8
JahanviJeswani <jahanvijeswani@Deveshs-MacBook-Air.local>
2022-12-28 20:25:58 +0530

    Updates

---
+++ precache-manifest.4f54416c9671ba7a2de49dcfcfbd5000.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "175e5118cc8c59fece96",
+    "url": "/NotesApp-React/static/css/main.241418b8.chunk.css"
+  },
+  {
+    "revision": "175e5118cc8c59fece96",
+    "url": "/NotesApp-React/static/js/main.175e5118.chunk.js"
+  },
+  {
+    "revision": "916436fb26a6bb0a3644",
+    "url": "/NotesApp-React/static/js/1.916436fb.chunk.js"
+  },
+  {
+    "revision": "d7beff0068f411bab5d1",
+    "url": "/NotesApp-React/static/js/runtime~main.d7beff00.js"
+  },
+  {
+    "revision": "43de55123c29bac576265e3d93403521",
+    "url": "/NotesApp-React/index.html"
+  }
+];
